72b074911cb0704f3f37dd43ba9666be15a4b32a
Julian Camilo Benavides Vanegas <julian.127@teleperformance.com>
2023-03-07 21:30:39 -0500

    api auth 90%

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,7 +1,47 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Account = require("../models/account.model");
+const saltRounds = 10;
+const { PRIVATE_KEY } = process.env;
 
-const login = (req, res) => {
+const login = async (req, res) => {
   try {
+    const user = req.body;
+
+    const userDB = await Account.findOne({ email: user.email });
+
+    if (!userDB) {
+      return res.status(404).json({
+        ok: false,
+        message: "User doesn´t exist, please signup",
+      });
+    }
+
+    console.log(userDB);
+
+    const passwordMatch = await bcrypt.compare(user.password, userDB.password);
+    console.log(passwordMatch);
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        ok: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        email: userDB.email,
+        fullName: userDB.fullName,
+      },
+      PRIVATE_KEY
+    );
+
+    return res.status(200).json({
+      ok: true,
+      token,
+    });
+    
   } catch (error) {
     res.send(error.message);
   }
@@ -20,11 +60,24 @@ const signup = async (req, res) => {
       });
     }
 
+    const passwordHash = await bcrypt.hash(user.password, saltRounds);
+
+    user.password = passwordHash;
     await Account.create(user);
-    return res.status(201).json({
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        user: user.fullName,
+      },
+      PRIVATE_KEY
+    );
+
+     return res.status(201).json({
       ok: true,
-      message: "Account created sucessfully",
+      token,
     });
+
   } catch (error) {
     return res.status(400).json({
       ok: false,
